ce43679504c6bc25ec08a490ac4822e425e09d66
Joshua Vial <joshua@enspiral.com>
2015-10-13 09:45:12 +1300

    ensure login redirects

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -10,14 +10,21 @@ RSpec.describe SiteController, type: :controller do
 
   describe 'POST #sign_in' do
     let!(:user) { FactoryGirl.create(:user)}
-    it 'sets current user if successful' do
-      post :sign_in, user: {name: user.name, password: user.password}
-      expect(session[:current_user_id]).to be(user.id)
+
+    context "legit sign in" do
+      before { post :sign_in, user: {name: user.name, password: user.password} }
+      it 'sets current user if successful' do
+        expect(session[:current_user_id]).to be(user.id)
+      end
+      it 'redirects to index page' do
+        expect(response).to redirect_to(root_path)
+      end
     end
-    it 'sets flash error if unsuccessful' do
-      post :sign_in, user: {text: 'some garbage'}
-      expect(flash[:error]).to_not be_nil
+    context "invalid sign in" do
+      before { post :sign_in, user: {text: 'some garbage'} }
+      it 'sets flash error if unsuccessful' do
+        expect(flash[:error]).to_not be_nil
+      end
     end
-    it 'redirects to index page'
   end
 end
